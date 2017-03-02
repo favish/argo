1a639aa02b691fadbbdfd02223d8bbd098e9d3b9
aguilarm <mika@favish.com>
2017-03-02 13:49:08 -0600

    feat(sync): Add database sync

--- cmd/project/sync.go
+++ cmd/project/sync.go
@@ -26,26 +26,117 @@ var filesCmd = &cobra.Command{
 	Valid choices for FROM/TO are local, prod, or dev.
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
-
 		validateArgs(args)
 
-		from, to := getSyncPaths(args)
+		source, destination := getFileSyncPaths(args)
 
-		if approve := util.GetApproval(fmt.Sprintf("This will sync files from %s (%s) to %s (%s), proceed?", args[0], from, args[1], to)); !approve {
+		if approve := util.GetApproval(fmt.Sprintf("This will sync files from %s (%s) to %s (%s), proceed?", args[0], source, args[1], destination)); !approve {
 			os.Exit(1)
 		} else {
-			color.Yellow("Adding ssh aliases via gcloud for compute instances...")
+			color.Cyan("Adding ssh aliases via gcloud for compute instances...")
 
-			util.ExecCmd("gcloud", "compute", "config-ssh")
+			util.ExecCmd("gcloud", "compute", "config-ssh", "--project=favish-general")
 
-			util.ExecCmd("rsync", "-avz", from, to)
+			util.ExecCmd("rsync", "-avz", fmt.Sprintf("%s*", source), destination)
+		}
+	},
+}
+
+var dbCmd = &cobra.Command{
+	Use: "db",
+	Short: "Sync database FROM > TO.",
+	Long: `Use this command to retrieve or push your database to and from remote environments.
+	Ex. "argo project sync db dev local" brings dev db to your machine.
+	Light wrapper around drush, implementation similar to drush sql-sync in a world without ssh.
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		validateArgs(args)
+
+		if approve := util.GetApproval("Syncing databases will cause a temporary service outage for both targets, are you sure?"); !approve {
+			return
+		}
+
+		source := args[0]
+		destination := args[1]
+
+		// Process:
+		// Connect to SOURCE application pod
+		// Dump database to temp file
+		// Copy database to developer's machine
+		// Unzip local copy
+		// Connect to DESTINATION application pod
+		// Create temp backup of DESTINATION database
+		// Drop DESTINATION application database
+		// Sync DESTINATION application database
+		// Delete developer's temp file
+
+		// Point kubectl at FROM, dump database to local temp file
+		setKubectlConfig(source)
+		// Get the container running application
+		sourcePodName, err := util.ExecCmdChain("kubectl get pods --output='name' --selector='service=app' | awk -F'/' '{print $2}' | tr -d '\n'")
+		if (err != nil) {
+			color.Red("Error getting pod name: %s", err)
+			os.Exit(1)
+		}
+
+		_, err = util.ExecCmdChain(fmt.Sprintf("kubectl exec --stdin=true --tty=true %s -- /bin/bash -c 'cd docroot && drush sql-dump --gzip --result-file=/tmp/argo-db-tmp.sql'", sourcePodName))
+		if (err != nil) {
+			color.Red("Error getting dump: %s", err)
+			os.Exit(1)
+		}
+
+		err = util.ExecCmd("kubectl", "cp", fmt.Sprintf("%s:/tmp/argo-db-tmp.sql.gz", sourcePodName), "/tmp/argo-db-tmp.sql.gz")
+		if (err != nil) {
+			color.Red("Error copying dump locally: %s", err)
+			os.Exit(1)
+		}
+
+		color.Cyan("Unzipping.  You may be prompted to overwrite, please do so.")
+		err = util.ExecCmd("gunzip", "/tmp/argo-db-tmp.sql.gz")
+		if (err != nil) {
+			color.Red("Error unzipping database dump: %s", err)
+			os.Exit(1)
+		}
+
+		color.Cyan("Redirecting kubectl config to %s environment...", destination)
+		setKubectlConfig(destination)
+		// Get the container running application on destination
+		destPodName, err := util.ExecCmdChain("kubectl get pods --output='name' --selector='service=app' | awk -F'/' '{print $2}' | tr -d '\n'")
+		if (err != nil) {
+			color.Red("Error getting pod name: %s", err)
+			os.Exit(1)
+		}
+
+		color.Cyan("Creating backup of %s before dropping existing database...", destination)
+		_, err = util.ExecCmdChain(fmt.Sprintf("kubectl exec --stdin=true --tty=true %s -- /bin/bash -c 'cd docroot && drush sql-dump --gzip --result-file=sites/default/files/argo-db-tmp.sql.bak'", destPodName))
+		if (err != nil) {
+			color.Red("Error backing up: %s", err)
+			os.Exit(1)
+		}
+
+		//color.Cyan("Dropping %s database...", destination)
+		//err = util.ExecCmd("kubectl", "exec", "--stdin=true", "--tty=true", destPodName, "--", "/bin/bash -c \"cd docroot && drush sql-drop -y\"")
+
+		color.Cyan("Importing database dump to %s environment, may take a few moments...", destination)
+		_, err = util.ExecCmdChain(fmt.Sprintf("kubectl exec --stdin=true --tty=true %s < /tmp/argo-db-tmp.sql -- /bin/bash -c 'cd docroot && drush sqlc'", destPodName))
+		if (err != nil) {
+			color.Red("Error importing database: %s", err)
+			os.Exit(1)
+		}
+
+		err = util.ExecCmd("rm", "/tmp/argo-db-tmp.sql")
+		if (err != nil) {
+			color.Red("Error removing temp database: %s", err)
+			os.Exit(1)
 		}
 	},
 }
 
 func init() {
 	syncCmd.AddCommand(filesCmd)
+	syncCmd.AddCommand(dbCmd)
 }
+
 
 func validateArgs(args []string) {
 	if (len(args) > 2) {
@@ -66,11 +157,11 @@ func validateArgs(args []string) {
 	}
 }
 
-func getSyncPaths(args []string) (string, string) {
+func getFileSyncPaths(args []string) (string, string) {
 	projectName := projectConfig.GetString("project-name")
 	locations := map[string]string {
-		"dev": fmt.Sprintf("%s:%s%s/*", viper.GetString("environments.dev.files-instance"), "/mnt/disks/", projectName),
-		"prod": fmt.Sprintf("%s:%s%s/*", viper.GetString("environments.prod.files-instance"), "/mnt/disks/", projectName),
+		"dev": fmt.Sprintf("%s:%s%s/", viper.GetString("environments.dev.files-instance"), "/mnt/disks/", projectName),
+		"prod": fmt.Sprintf("%s:%s%s/", viper.GetString("environments.prod.files-instance"), "/mnt/disks/", projectName),
 		"local": "./docroot/sites/default/files/",
 	}
 
